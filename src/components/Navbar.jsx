2d1cb44b0a4e53ba90c260a0c53db6d7d14f87f1
Brayan Garcia <bgarcia.95@outlook.com>
2023-04-10 11:11:58 -0600

    use ActiveLink and apply styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
-import Link from "next/link";
+import { ActiveLink } from "./ActiveLink";
+import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
   return (
-    <nav>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/contact">Contact</Link>
+    <nav className={styles["menu-container"]}>
+      <ActiveLink text="Home" href="/" />
+      <ActiveLink text="About" href="/about" />
+      <ActiveLink text="Contact " href="/contact" />
     </nav>
   );
 };
