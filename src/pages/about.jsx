3023ee69f00521171a739fffc4fe1d2e48da3a9f
Brayan Garcia <bgarcia.95@outlook.com>
2023-04-10 11:12:51 -0600

    refactor about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,35 +1,30 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import Link from "next/link";
-import { Navbar } from "@/components/Navbar";
+
+import { MainLayout } from "@/components/layouts/MainLayout";
+import { DarkLayout } from "@/components/layouts/DarkLayout";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function AboutPage() {
   return (
     <>
-      <Head>
-        <title>About</title>
-        <meta name="description" content="About Page" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+      <h1>About Page</h1>
 
-      <Navbar />
-
-      <main className={styles.main}>
-        <h1>About Page</h1>
-
-        <h1 className={styles.title}>
-          Go to <Link href="/">Home</Link>
-        </h1>
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.jss</code>
-        </p>
-      </main>
+      <h1 className={"title"}>
+        Go to <Link href="/">Home</Link>
+      </h1>
+      <p className={"description"}>
+        Get started by editing <code className={"code"}>pages/about.jsx</code>
+      </p>
     </>
   );
 }
+
+AboutPage.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>{page}</DarkLayout>
+    </MainLayout>
+  );
+};
